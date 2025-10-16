d035bc88bc42980a436ba661fc38479f95418d95
Denis Neves <nevesdenis35@gmail.com>
2025-10-16 17:54:23 -0300

    build do projeto

--- app.config.ts
+++ app.config.ts
@@ -6,6 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "checkin",
   slug: "checkin",
   scheme: "checkin",
+  owner: "fxneves",
   version: "1.0.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
@@ -27,8 +28,11 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     },
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
+    package: "com.fxneves.checkin",
   },
   plugins: [
+    "expo-router",
+    "expo-font",
     [
       "expo-location",
       {
@@ -40,5 +44,14 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   extra: {
     supabaseUrl: process.env.SUPABASE_URL,
     supabaseKey: process.env.SUPABASE_KEY,
+    eas: {
+      projectId: "3a6dda48-db1b-49ca-88f9-8d54c6c1ac58",
+    },
+  },
+  updates: {
+    url: "https://u.expo.dev/3a6dda48-db1b-49ca-88f9-8d54c6c1ac58",
+  },
+  runtimeVersion: {
+    policy: "appVersion",
   },
 });
